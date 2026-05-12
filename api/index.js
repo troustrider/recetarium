691335d0f1c3aedd4764c5c90439651c5c371596
Karim Abatouy <akarim1398@gmail.com>
2026-05-12 13:57:25 +0200

    fix: incluir server/package.json en bundle de Vercel para resolver CJS

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const app = require('../server/src/index.js')
+import app from '../server/src/index.js'
 export default app
